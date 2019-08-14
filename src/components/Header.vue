2f1d22a3ef2601548930d54a00f65adde6c5b508
Stanley Huang <huang.stanley050@gmail.com>
2019-08-14 14:25:05 +1000

    added some media query to nav links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,31 @@
 <template>
   <div>
     <v-navigation-drawer v-model="sideNav" absolute temporary>
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>John Leider</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <v-list>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>
+              mdi-steam
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>View Meetups</v-list-item-title>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <div>
       <v-toolbar>
-        <v-app-bar-nav-icon @click="sideNav = !sideNav"></v-app-bar-nav-icon>
-        <v-toolbar-title>AnimeMeetup</v-toolbar-title>
+        <v-app-bar-nav-icon
+          class="d-sm-none"
+          @click="sideNav = !sideNav"
+        ></v-app-bar-nav-icon>
+        <v-toolbar-title class="d-none d-sm-flex d-lg-flex d-xl-flex d-md-flex"
+          >AnimeMeetup</v-toolbar-title
+        >
 
         <v-spacer> </v-spacer>
-        <v-toolbar-items>
-          <v-btn text><v-icon left>mdi-home</v-icon>View Meetups</v-btn>
+        <v-toolbar-items class="d-none d-sm-flex d-lg-flex d-xl-flex d-md-flex">
+          <v-btn text><v-icon left>mdi-steam</v-icon>View Meetups</v-btn>
         </v-toolbar-items>
       </v-toolbar>
     </div>
