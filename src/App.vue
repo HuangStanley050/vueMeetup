0a222c8ef7534a2248c2ee23b54db05469dd49c4
Stanley Huang <huang.stanley050@gmail.com>
2019-08-14 13:44:45 +1000

    outsourced navbar

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,19 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer v-model="sideNav" absolute temporary>
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>John Leider</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <div>
-      <v-toolbar>
-        <v-toolbar-title
-          :style="{ cursor: pointer }"
-          @click="sideNav = !sideNav"
-          >AnimeMeetup</v-toolbar-title
-        >
-        <v-spacer> </v-spacer>
-        <v-toolbar-items>
-          <v-btn text><v-icon left>mdi-home</v-icon>View Meetups</v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-    </div>
-
+    <Header />
     <main>
       Hello world
     </main>
   </v-app>
 </template>
 <script>
+import Header from "./components/Header.vue";
 export default {
   data() {
     return {
       sideNav: false
     };
   },
-  components: {}
+  components: { Header }
 };
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="sideNav" absolute temporary>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+
+    <div>
+      <v-toolbar>
+        <v-app-bar-nav-icon @click="sideNav = !sideNav"></v-app-bar-nav-icon>
+        <v-toolbar-title>AnimeMeetup</v-toolbar-title>
+
+        <v-spacer> </v-spacer>
+        <v-toolbar-items>
+          <v-btn text><v-icon left>mdi-home</v-icon>View Meetups</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      sideNav: false
+    };
+  }
+};
+</script>
