0579f0f472ebfee03cc83d28d543f43ccebe62cf
Stanley Huang <huang.stanley050@gmail.com>
2019-09-05 20:21:59 +1000

    did some work on the modal component, made a loader to ensure data loads

--- src/components/Meetups/MeetUp.vue
+++ src/components/Meetups/MeetUp.vue
@@ -1,12 +1,22 @@
 <template>
   <v-container>
-    <v-row wrap>
+    <v-row v-if="isLoading">
+      <v-col xs="12" class="text-center">
+        <v-progress-circular
+          :width="7"
+          :size="50"
+          indeterminate
+          color="primary"
+        ></v-progress-circular>
+      </v-col>
+    </v-row>
+    <v-row wrap v-else>
       <v-col xs="12" sm="10" md="8" offset-sm="1" offset-md="2">
         <v-card>
           <v-card-title>
             <h5 class="info--text">{{ meetup.title }}</h5>
-            <template
-              ><v-spacer> </v-spacer><MeetupEditModal />
+            <template v-if="userIsCreator"
+              ><v-spacer> </v-spacer><MeetupEditModal :meetup="meetup" />
             </template>
           </v-card-title>
           <v-img :src="meetup.imageUrl" class="white--text" height="300px" />
@@ -38,6 +48,21 @@ export default {
     MeetupEditModal
   },
   computed: {
+    isLoading() {
+      return this.$store.getters.loading;
+    },
+    userIsCreator() {
+      if (!this.userIsAuthenticated) {
+        return false;
+      }
+      return this.$store.getters.user.id === this.meetup.creatorId;
+    },
+    userIsAuthenticated() {
+      return (
+        this.$store.getters.user !== null &&
+        this.$store.getters.user !== undefined
+      );
+    },
     meetup() {
       return this.$store.getters.loadedMeetup(this.id);
     }
--- src/components/Meetups/edit/EditMeetUpModal.vue
+++ src/components/Meetups/edit/EditMeetUpModal.vue
@@ -9,15 +9,60 @@
           Edit
         </v-btn>
       </template>
+      <v-card>
+        <v-container>
+          <v-row wrap>
+            <v-col xs="12">
+              <v-card-title>
+                Edit Meetup
+              </v-card-title>
+            </v-col>
+          </v-row>
+          <v-divider></v-divider>
+          <v-row wrap>
+            <v-col xs="12">
+              <v-card-text>
+                <v-text-field
+                  label="Title"
+                  v-model="editedTitle"
+                  id="title"
+                  name="title"
+                  required
+                ></v-text-field>
+                <v-textarea
+                  label="Description"
+                  id="description"
+                  v-model="editedDescription"
+                  name="description"
+                  required
+                ></v-textarea>
+              </v-card-text>
+            </v-col>
+          </v-row>
+          <v-divider> </v-divider>
+
+          <v-row>
+            <v-col xs="12">
+              <v-card-actions>
+                <v-btn text color="blue">Accept Changes</v-btn>
+                <v-btn outlined color="red">Cancel Changes</v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
     </v-dialog>
   </div>
 </template>
 
 <script>
 export default {
+  props: ["meetup"],
   data() {
     return {
-      dialog: false
+      dialog: false,
+      editedTitle: this.meetup.title,
+      editedDescription: this.meetup.description
     };
   }
 };
